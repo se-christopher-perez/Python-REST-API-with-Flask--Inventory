7054ca374eeca979eb3544227593c70c5eacbdfa
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-13 13:05:00 -0400

    update created tests for geting product by id and posting a new product

--- testing/app_test.py
+++ testing/app_test.py
@@ -11,4 +11,23 @@ def test_get_inventory():
     if response.status_code != 200:
         return TypeError(f"error: {response.status_code}")
 
+def test_get_product():
+    response = app_test.get("/inventory/1")
+
+    if response.status_code != 200:
+        return TypeError(f"error: {response.status_code}")
     
+def test_add_product():
+    response = app_test.post("/inventory", json={
+
+        "product_name": "Oreos",
+        "brands": "Nabisco",
+        "ingredients": "Sugar, flour, cocoa...",
+        "quantity": "154g",
+        "price": 2.99,
+        "stock": 30
+
+    })
+
+    if response.status_code != 201:
+        return TypeError(f"error: {response.status_code}")
