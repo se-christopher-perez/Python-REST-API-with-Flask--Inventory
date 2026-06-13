7a025373c431b5a168351325504f947aeeeb31db
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-13 11:59:48 -0400

    created functions in the cli to PATCH and POST called add_new_product and update_product

--- cli.py
+++ cli.py
@@ -17,3 +17,29 @@ def get_product(id):
 
 get_product(2)
 
+def add_new_product(product_name, brands, ingredients, quantity, price, stock):
+    response = requests.post(f"{URL}/inventory", json={
+        
+        "product_name": product_name,
+        "brands": brands,
+        "ingredients": ingredients,
+        "quantity": quantity,
+        "price": price,
+        "stock": stock
+    
+    })
+
+    print(response.json())
+
+add_new_product("Oreos", "Nabisco", "Sugar, flour, cocoa", "154g", 2.99, 30)
+
+def update_product(id, price, stock):
+    response = requests.patch(f"{URL}/inventory/{id}", json={
+    
+        "price": price,
+        "stock": stock
+
+    })
+
+    print(response.json())
+
