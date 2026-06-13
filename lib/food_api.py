e89f273e94764138868ed49a28b7c672b8f6e70f
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-13 03:28:48 -0400

    Began, added folders and files, created the api commection to pull information based on barcode returning the product name, brands, and ingredients, and qty

---
+++ lib/food_api.py
@@ -0,0 +1,37 @@
+import requests
+
+HEADERS = {
+    "User-Agent": "MyApp/1.0 (myapp@example.com)"
+}
+
+class FoodAPI:
+
+    def search_barcode(self, barcode):
+        response = requests.get(
+            f"https://world.openfoodfacts.org/api/v0/product/{barcode}.json",
+            headers=HEADERS
+        ).json()
+
+        if response.status_code != 200:
+            print(f"Error: {response.status_code}")
+            return None
+
+        product = response["product"]
+
+        print(product)
+
+        return {
+            "product_name": product["product_name"],
+            "brands":       product["brands"],
+            "ingredients":  product["ingredients_text"],
+            "quantity":     product["quantity"]
+        }
+    
+nutella = FoodAPI().search_barcode("3017620422003")
+coke = FoodAPI().search_barcode("5449000000996")
+ketchup = FoodAPI().search_barcode("0013000006408")
+
+print("Search Result:\n")
+print(nutella)
+print(coke)
+print(ketchup)
