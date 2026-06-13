e21e84331b32fdf0fc867b9f03188edb0364dc79
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-13 04:05:44 -0400

    added route get_inventory

--- server/app.py
+++ server/app.py
@@ -1,5 +1,14 @@
+from flask import Flask, jsonify, request
 from data import inventory
+# from lib.food_api import FoodAPI
+
+app = Flask(__name__)
 
 data = inventory
 
-print(data)
+@app.route("/inventory")
+def get_inventory():
+    return jsonify(data), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
